ffca1d7dce5bf4ef76ce68e224e7441a2c1994dd
waabiMkwa <34587659+waabiMkwa@users.noreply.github.com>
2022-06-12 09:22:29 -0400

    Syncdataset 1 and first clone of DS.

--- synthesize_convos.py
+++ synthesize_convos.py
@@ -16,7 +16,7 @@ def save_convo(text, topic):
 openai.api_key = open_file('openaiapikey.txt')
 
 
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.85, top_p=1.0, tokens=1000, freq_pen=0.0, pres_pen=0.5, stop=['<<END>>']):  # NOTE: original temp was 0.7 and freq_pen was 0.0 - I turned these up to reduce repetition
+def gpt3_completion(prompt, engine='text-davinci-002', temp=0.85, top_p=1.0, tokens=1000, freq_pen=0.03, pres_pen=0.5, stop=['<<END>>']):  # NOTE: original temp was 0.7 and freq_pen was 0.0 - I turned these up to reduce repetition
     max_retry = 5
     retry = 0
     while True:
@@ -50,9 +50,9 @@ if __name__ == '__main__':
             print(topic)
             prompt = open_file('prompt.txt').replace('<<TOPIC>>', topic)
             response = gpt3_completion(prompt)
-            outtext = 'User: Hey TIM%s' % response
+            outtext = 'User: Hey KINOOMAAGANSE%s' % response
             print(outtext)
-            tpc = topic.replace(' ', '').replace('(','').replace(')','').replace('-','').replace(',','').replace('"')[0:15]
+            tpc = topic.replace(' ', '').replace('(','').replace(')','').replace('-','').replace(',','').replace('"', '')[0:15]
             save_convo(outtext, tpc)
             #exit()
             
